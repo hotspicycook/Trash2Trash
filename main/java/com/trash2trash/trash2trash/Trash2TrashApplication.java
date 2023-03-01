779fcb3eaca85eac32d7c89349c25722f2c9141e
scott1 <redonfire29@gmail.com>
2023-02-28 19:15:52 -0600

    the start of something fun

---
+++ main/java/com/trash2trash/trash2trash/Trash2TrashApplication.java
@@ -0,0 +1,13 @@
+package com.trash2trash.trash2trash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trash2TrashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trash2TrashApplication.class, args);
+	}
+
+}
---
+++ test/java/com/trash2trash/trash2trash/Trash2TrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trash2trash.trash2trash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trash2TrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
